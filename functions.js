7a8038a00bdbef77915f03d77341c11d98b94ac5
krisKurken <kristian.liljander@live.se>
2017-10-20 15:25:30 +0200

    Added functions
    Changed in html and added function in .js

--- functions.js
+++ functions.js
@@ -1,36 +1,8 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+function show_cockie(){
+  if (document.getElementById("vase1").style.opacity === "1") {
+    document.getElementById("vase1").style.opacity = 0;
+  }
+  else {
+    document.getElementById("vase1").style.opacity = 1;
+  }
+};
